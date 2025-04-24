3c57e0a62d9ffb3ae4bcd939e56b62980c0fd1f2
mazevedoc <melk.azevedo@outlook.com>
2025-04-24 12:52:13 -0300

    ajuste para deploy do back

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
-async function enviarEmail() {
+async function enviarContato() {
+    const nome = document.getElementById('nome').value;
     const email = document.getElementById('email').value;
+    const telefone = document.getElementById('telefone').value;
+    const mensagem = document.getElementById('mensagem').value;
 
     try {
-        const response = await fetch('LEMBRAR DE COLOCAR A URL DO BACK AQUI', {
+        const response = await fetch('https://desafio-html-dio.onrender.com', {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ email })
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ nome, email, telefone, mensagem })
         });
 
         const data = await response.json();
-        alert(data.message);
+        alert(data.message || "Contato enviado com sucesso!");
     } catch (error) {
-        console.error('Erro ao enviar:', error);
-        alert('Erro ao enviar o e-mail');
+        console.error('Erro ao enviar contato:', error);
+        alert('Erro ao enviar o contato');
     }
 }
