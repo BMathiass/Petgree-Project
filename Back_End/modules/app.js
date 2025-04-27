3d0cd6dde6438a84f0ad68ba32a3b917a75aebaf
mazevedoc <melk.azevedo@outlook.com>
2025-04-27 00:57:29 -0300

    ajuste no cors

--- Back_End/modules/app.js
+++ Back_End/modules/app.js
@@ -3,10 +3,16 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
+const corsOptions = {
+    origin: 'https://petgree.onrender.com',
+    methods: 'GET,POST',
+    allowedHeaders: 'Content-Type',
+};
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 module.exports = app;
